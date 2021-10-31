0310f311dde2665628d1331ded4fb9b9d91e7005
Cristian <cristian.fernandez123@yahoo.com>
2021-10-30 20:01:53 -0700

    added matreial UI and added the 3 columns layout

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../_app.scss";
+import ThreeColumns from "../components/threeColumns/threeColumns"; 
+
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <ThreeColumns />
+        </div>
+    )
+}
+
+export default App
---
+++ client/src/components/threeColumns/threeColumns.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./_threecolumns.scss"; 
+
+const threeColumns = () => {
+  return (
+    <div className="main-container">
+      <ul className="first-container"></ul>
+      <ul className="second-container"></ul>
+      <ul className="third-container"></ul>
+    </div>
+  );
+};
+
+export default threeColumns;
